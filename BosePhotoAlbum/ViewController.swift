939a320daf3263d12c9892666a140533babb9e6c
aavu <aavu@gatech.edu>
2019-03-17 11:30:54 +0530

    Initial Commit

---
+++ BosePhotoAlbum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BosePhotoAlbum
+//
+//  Created by Raghavasimhan Sankaranarayanan on 3/17/19.
+//  Copyright © 2019 Aavu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
